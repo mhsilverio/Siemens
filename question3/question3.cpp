abf0959fcb646c374d855d585f5ae28f93cd1688
mhsilverio <milenahsilverio@gmail.com>
2022-01-29 13:41:57 -0300

    update files

--- question3/question3.cpp
+++ question3/question3.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void ConcatERemove(string s, string t, int k){
+    if 
+        cout << "SIM" << endl;
+    else 
+        cout << "NÃO" << endl;
+}
+
 int main() {
+    string s, t;
+    int k = 0;
+
+    cout << "String s: "; cin >> s >> endl;
+    cout << "String t: "; cin >> t >> endl;
+    cout << "Movientos de k: "; cin >> k >> endl;
+    
+    ConcatERemove(s,t,k);
+
+    return 0;
 }
