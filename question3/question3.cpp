0e6fc75fa8455cf8ea7b26d324412da6f0690d37
mhsilverio <milenahsilverio@gmail.com>
2022-01-31 00:10:15 -0300

    not finished

--- question3/question3.cpp
+++ question3/question3.cpp
@@ -1,22 +1,54 @@
+// NOT FINISHED 
 #include <iostream>
 using namespace std;
 
 void ConcatERemove(string s, string t, int k){
-    if 
-        cout << "SIM" << endl;
-    else 
-        cout << "NÃO" << endl;
+    int menor, maior;
+    int movimentos;
+
+    // PRIMEIRAMENTE É NECESSÁRIO OBSERVAR O TAMANHO DAS STRINGS EXISTENTES
+    if (s.size() > t.size()){
+        menor = t.size();
+        maior = s.size();
+        cout << "\nMenor string: " << t << endl;
+        cout << "Maior string: " << s << endl;
+    }
+    else if (s.size() < t.size()){
+        menor = s.size();
+        maior = t.size();
+        cout << "\nMenor string: " << s << endl;
+        cout << "Maior string: " << t << endl;
+    } 
+    else if (maior = menor = t.size()){
+        cout << "\nAs strings contem os mesmos tamanhos" << endl;
+    }
+    
+    movimentos = (s.size() - t.size());
+
+    for (int i = 0; i < menor; i++){
+        if (s[i] != t[i]){
+            if(i == 0 || maior <= (k - menor)){
+                movimentos = movimentos + ((menor - i));
+                while (movimentos != k){
+                    ++movimentos;
+                }
+            } else movimentos = movimentos + ((menor - i));
+        }
+    }
+    if (movimentos == k){
+        cout << "\nSIM"<< endl;
+    } else cout << "\nNAO" << endl;
 }
 
 int main() {
     string s, t;
     int k = 0;
 
-    cout << "String s: "; cin >> s >> endl;
-    cout << "String t: "; cin >> t >> endl;
-    cout << "Movientos de k: "; cin >> k >> endl;
-    
-    ConcatERemove(s,t,k);
+    cout << "\nString s: " ; cin >> s;
+    cout << "String t: "; cin >> t;
+    cout << "Movimentos de k: "; cin >> k;
+
+    ConcatERemove(s, t, k);
 
     return 0;
 }
